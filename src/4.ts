7623c17a8d71985a1e83add5b2c08cc2956ec00e
OlesiaZaozirna <137696365+OlesiaZaozirna@users.noreply.github.com>
2024-02-22 23:10:15 +0200

    update

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,49 @@
+class Key {
+  private signature: number = Math.random();
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`${person.getKey()} is coming in`);
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('Opening the door');
+    } else {
+      console.log('You do not have the correct key');
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
